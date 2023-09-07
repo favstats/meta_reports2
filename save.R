73e819b2f88053d5994b6220cfc7f31454d7e5c3
Fabio Votta <fabio.votta@gmail.com>
2023-09-07 09:30:22 +0200

    Update save.R

--- save.R
+++ save.R
@@ -3,16 +3,16 @@
 source("use.R")
 
 # install.packages("pacman")
-pacman::p_load(
-  janitor,
-  tidyr,
-  dplyr,
-  stringr,
-  lubridate,
-  purrr,
-  glue,
-  ggplot2
-)
+#pacman::p_load(
+#  janitor,
+#  tidyr,
+#  dplyr,
+#  stringr,
+#  lubridate,
+#  purrr,
+#  glue,
+#  ggplot2
+#)
 
 
 # daily_dat <- readRDS("data/daily.rds")
@@ -35,21 +35,21 @@ pacman::p_load(
 
 
 
-old_dat <- dir("lifelong", full.names = T) %>% 
-  keep(~str_detect(.x, "rds")) %>%
-  map_dfr_progress(readRDS)
+#old_dat <- dir("lifelong", full.names = T) %>% 
+#  keep(~str_detect(.x, "rds")) %>%
+#  map_dfr_progress(readRDS)
 
-old_dat %>%
-  mutate(date_produced = lubridate::ymd(date)) %>%
-  drop_na(date_produced) %>% 
-  janitor::clean_names() %>% 
-  distinct(cntry, date_produced, .keep_all = T) %>% 
-  count(date_produced) %>%
-  ggplot(aes(date_produced, n)) +
-  geom_col() +
-  theme_minimal() +
-  labs(y = "How many Countries", x = "For each date", title = paste0("Lifelong Reports: ", Sys.time()))
+#old_dat %>%
+#  mutate(date_produced = lubridate::ymd(date)) %>%
+#  drop_na(date_produced) %>% 
+#  janitor::clean_names() %>% 
+#  distinct(cntry, date_produced, .keep_all = T) %>% 
+#  count(date_produced) %>%
+#  ggplot(aes(date_produced, n)) +
+#  geom_col() +
+#  theme_minimal() +
+#  labs(y = "How many Countries", x = "For each date", title = paste0("Lifelong Reports: ", Sys.time()))
 
-ggsave("overview_ll.png", width = 8, height = 5)
+#ggsave("overview_ll.png", width = 8, height = 5)
 
 
